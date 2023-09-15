d5504c369ba3cecb948b7d2578e19bbdc5409b79
Paul Chambaz <paul.chambazz@gmail.com>
2023-09-15 17:40:47 +0200

    added personal conf

--- src/config.def.hpp
+++ src/config.def.hpp
@@ -10,16 +10,14 @@ constexpr int paddingX = 10;
 constexpr int paddingY = 3;
 
 // See https://docs.gtk.org/Pango/type_func.FontDescription.from_string.html
-constexpr const char* font = "Sans 12";
+constexpr const char* font = "SauceCodeProNerdFontMono-Regular 12";
 
 constexpr ColorScheme colorInactive = {Color(0xbb, 0xbb, 0xbb), Color(0x22, 0x22, 0x22)};
-constexpr ColorScheme colorActive = {Color(0xee, 0xee, 0xee), Color(0x00, 0x55, 0x77)};
-constexpr const char* termcmd[] = {"foot", nullptr};
+constexpr ColorScheme colorActive = {Color(0xee, 0xee, 0xee), Color(0x44, 0x44, 0x44)};
+constexpr const char* termcmd[] = {"footclient", nullptr};
 
 static std::vector<std::string> tagNames = {
-	"1", "2", "3",
-	"4", "5", "6",
-	"7", "8", "9",
+	"1", "2", "3", "4", "5", "6", "7", "8", "9",
 };
 
 constexpr Button buttons[] = {
