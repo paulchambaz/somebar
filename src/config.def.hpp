ef9701e79a831ba6a80a989ebe67d9c0d3f86100
Paul Chambaz <paul.chambazz@gmail.com>
2023-09-28 02:41:07 +0200

    revert font size

--- src/config.def.hpp
+++ src/config.def.hpp
@@ -9,7 +9,7 @@ constexpr int paddingX = 10;
 constexpr int paddingY = 3;
 
 // See https://docs.gtk.org/Pango/type_func.FontDescription.from_string.html
-constexpr const char* font = "SauceCodePro Nerd Font Mono 14";
+constexpr const char* font = "SauceCodePro Nerd Font Mono 12";
 
 constexpr ColorScheme colorInactive = {Color(0xbb, 0xbb, 0xbb), Color(0x22, 0x22, 0x22)};
 constexpr ColorScheme colorActive = {Color(0xee, 0xee, 0xee), Color(0x44, 0x44, 0x44)};
